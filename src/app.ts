b68444c99fcb03ddb7f838ea5fb69b534445220a
Pierre Repetto-Andipatin <pierre@anthillsolutions.fr>
2018-10-22 11:50:16 +0200

    refactor: move doc in server, remove useless files

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import * as bodyParser from 'body-parser'
 import * as express from 'express'
 import * as methodOverride from 'method-override'
 import * as morgan from 'morgan'
-import * as swaggerUi from 'swagger-ui-express'
 
 import { apiErrorHandler } from './middlewares/api-error-middleware'
 import { RegisterRoutes } from './routes'
@@ -10,14 +9,11 @@ import { RegisterRoutes } from './routes'
 import './controllers/users-controller'
 
 const app = express()
-const swaggerDocument = require(`${__dirname}/swagger.json`)
 
 // istanbul ignore next
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('combined'))
 }
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose'
+import * as swaggerUi from 'swagger-ui-express'
 
 import app from './app'
 
@@ -7,6 +8,10 @@ const DEFAULT_MONGODB_URI = 'mongodb://localhost:27017/demo-manager'
 
 const PORT = process.env.PORT || DEFAULT_PORT
 const MONGODB_URI = process.env.MONGODB_URI || DEFAULT_MONGODB_URI
+
+// Add swagger documentation
+const swaggerDocument = require(`${__dirname}/swagger.json`)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 console.log('Connecting to database...')
 mongoose.Promise = global.Promise
